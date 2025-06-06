4f4ebf2af0aaf5df4159f071541767c87c2c659b
Eduardo Castillo <eduardo.infoline@gmail.com>
2025-06-06 09:14:02 -0500

    Fix logic in form submission validation in AuthScreen

--- lib/screen/auth.dart
+++ lib/screen/auth.dart
@@ -19,7 +19,7 @@ class _AuthScreenState extends State<AuthScreen> {
   void _submit() {
     final isValid = _form.currentState!.validate();
 
-    if (!isValid) {
+    if (isValid) {
       _form.currentState!.save();
       print(_enteredEmail);
       print(_enteredPassword);
