22b7a755f0e1321eb9146a3eee9632d712b4c6c5
Eduardo Castillo <eduardo.infoline@gmail.com>
2025-06-07 07:56:29 -0500

    Add ChatMessages widget to display a message when no messages are found

---
+++ lib/widgets/chat_messenges.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ChatMessenges extends StatelessWidget {
+  const ChatMessenges({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('No messages found.'));
+  }
+}
