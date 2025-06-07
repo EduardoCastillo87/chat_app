e78276b40e76e5fce89e60e3aa891378165a30ae
Eduardo Castillo <eduardo.infoline@gmail.com>
2025-06-07 07:56:46 -0500

    Implement loading state in authentication flow by adding SplashScreen during Firebase auth state changes in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:chat_app/screen/splash.dart';
 import 'package:flutter/material.dart';
 import 'package:chat_app/screen/auth.dart';
+import 'package:chat_app/screen/splash.dart';
 import 'package:chat_app/screen/chat.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -26,6 +28,10 @@ class App extends StatelessWidget {
       home: StreamBuilder(
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const SplashScreen();
+          }
+
           if (snapshot.hasData) {
             return const ChatScreen();
           }
