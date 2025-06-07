e5159e7c7e743a412d4a4dcfd03997fcb6c9e1a0
Eduardo Castillo <eduardo.infoline@gmail.com>
2025-06-06 23:36:18 -0500

    Add ChatScreen widget with Firebase sign-out functionality

---
+++ lib/screen/chat.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('FlutterChat'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            icon: const Icon(Icons.exit_to_app),
+          ),
+        ],
+      ),
+      body: const Center(child: Text('Chat Screen')),
+    );
+  }
+}
