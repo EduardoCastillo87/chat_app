22fff0e6901cb0431569b4503a06394a73945b94
Eduardo Castillo <eduardo.infoline@gmail.com>
2025-06-07 07:56:41 -0500

    Refactor ChatScreen to include ChatMessages and NewMessage widgets, enhancing the chat interface and user experience.

--- lib/screen/chat.dart
+++ lib/screen/chat.dart
@@ -1,3 +1,5 @@
+import 'package:chat_app/widgets/chat_messenges.dart';
+import 'package:chat_app/widgets/new_message.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -14,11 +16,14 @@ class ChatScreen extends StatelessWidget {
             onPressed: () {
               FirebaseAuth.instance.signOut();
             },
-            icon: const Icon(Icons.exit_to_app),
+            icon: Icon(
+              Icons.exit_to_app,
+              color: Theme.of(context).colorScheme.primary,
+            ),
           ),
         ],
       ),
-      body: const Center(child: Text('Chat Screen')),
+      body: Column(children: [ChatMessenges(), NewMessage()]),
     );
   }
 }
